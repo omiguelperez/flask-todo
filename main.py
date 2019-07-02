b94b4d6815347163c686de3b24eb74431577b355
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-01 22:06:17 -0500

    Hello world with templates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Flask app."""
 
-from flask import Flask, request, redirect, make_response
+from flask import Flask, request, redirect, make_response, render_template
 
 app = Flask(__name__)
 
@@ -9,18 +9,21 @@ app = Flask(__name__)
 def index():
     """Set user ip in browser cookie and redirect hello view."""
     user_ip = request.remote_addr
-
     response = make_response(redirect('/hello'))
     response.set_cookie('user_ip', user_ip)
-
     return response
 
 
 @app.route('/hello')
 def hello():
     """Show greeting."""
-    user_ip  = request.cookies['user_ip']
-    return 'Hello! Your ip is {}'.format(user_ip)
+    user_ip  = request.cookies.get('user_ip', None)
+    todos = ['Todo #1', 'Todo #2', 'Todo #3']
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.html', **context)
 
 
 if __name__ == "__main__":
