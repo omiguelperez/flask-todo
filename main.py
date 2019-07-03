125d11c802c2cdd6a9b46e9b6111778f1cac7f2b
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-03 15:29:19 -0500

    Flask sessions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """Flask app."""
 
-from flask import Flask, request, redirect, make_response, render_template
+from flask import Flask, request, redirect, make_response, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 boostrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = '<zlUxe@t6JO&[ngx@@}*dimYQ6&h,y'
 
 
 @app.errorhandler(404)
@@ -18,14 +20,14 @@ def index():
     """Set user ip in browser cookie and redirect hello view."""
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
 
 @app.route('/hello')
 def hello():
     """Show greeting."""
-    user_ip = request.cookies.get('user_ip', None)
+    user_ip = session.get('user_ip', None)
     todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
     context = {
         'user_ip': user_ip,
@@ -34,5 +36,5 @@ def hello():
     return render_template('hello.html', **context)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
