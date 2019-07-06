86c92be7021f5a6b6a49931f13b32126b29a24e4
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-06 00:27:40 -0500

    Signup and update login with password hash verification

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,9 @@
 
 from flask import redirect, flash, url_for, render_template, session
 from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.forms import LoginForm
 from app.models import UserData, UserModel
 
@@ -37,7 +38,7 @@ def login():
         if user_doc.to_dict() is not None:
             password_from_db = user_doc.to_dict()['password']
 
-            if password_from_db == password:
+            if check_password_hash(password_from_db, password):
                 user_data = UserData(username, password)
                 user = UserModel(user_data)
 
@@ -53,3 +54,37 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    """Create an account."""
+    signup_form = LoginForm()
+
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+
+            user_data = UserData(username=username, password=password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Welcome {}. Starts with your account.'.format(username))
+
+            return redirect(url_for('hello'))
+        else:
+            flash('Username already exists.')
+
+    return render_template('signup.html', **context)
--- app/firestore_service.py
+++ app/firestore_service.py
@@ -20,6 +20,12 @@ def get_users():
     return db.collection('users').get()
 
 
+def user_put(user_data):
+    """Create an user on database."""
+    user_ref = db.collection('users').document(user_data.username)
+    user_ref.set({'password': user_data.password})
+
+
 def get_todos(username):
     """Get todos filtered by username."""
     return db.collection('users').document(username).collection('todos').get()
